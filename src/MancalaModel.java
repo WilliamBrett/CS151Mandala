c1e13e99d7b3dbd5c8fd87c6b538986e62fcccc2
Jeffrey Huynh <huynhjeffrey50@gmail.com>
2017-11-25 15:30:04 -0800

    update to mancalaModel

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,4 +1,21 @@
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.*;
 
 public class MancalaModel {
 
+	private ArrayList<ChangeListener> theListeners;
+	private int theData[];
+	
+	
+	MancalaModel(int[] theData)
+	{
+		this.theData = theData;
+		
+	}
+	
 }
