e32cbcb951bd431fb6e915d2b1738c04ea373890
Jeffrey Huynh <huynhjeffrey50@gmail.com>
2017-11-25 15:51:38 -0800

    finished mancalaModel

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -11,11 +11,32 @@ public class MancalaModel {
 	private ArrayList<ChangeListener> theListeners;
 	private int theData[];
 	
-	
+	/*
+	 * this is the model class
+	 */
+	//make sure when MancalaModel is created, the array is of size 14
 	MancalaModel(int[] theData)
 	{
 		this.theData = theData;
-		
+		theListeners = new ArrayList<ChangeListener>();
+	}
+	
+	public int[] getData()
+	{
+		return theData;
+	}
+	
+	public void attach(ChangeListener e)
+	{
+		theListeners.add(e);
+	}
+	
+	public void update()
+	{
+		for(ChangeListener e: theListeners)
+		{
+			e.stateChanged(new ChangeEvent(this));
+		}
 	}
 	
 }
