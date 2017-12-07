2128e670f45050df7899b5b139cdd22a9884d0da
Jeffrey Huynh <huynhjeffrey50@gmail.com>
2017-12-06 19:12:44 -0800

    test

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -450,3 +450,138 @@ public class MancalaModel {
 		return errorMsg;
 	}
 }
+
+/*
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.*;
+
+public class MancalaModel {
+
+	private ArrayList<ChangeListener> theListeners;
+
+	private int currentData[];
+	private int oldData[];
+
+	
+	
+	MancalaModel()
+	{
+		currentData = new int[14];
+		oldData = currentData;
+		theListeners = new ArrayList<ChangeListener>();
+	}
+	
+	public int[] getCurrentData()
+	{
+		return currentData;
+	}
+	
+	public int[] getOldData()
+	{
+		return oldData;
+	}
+	public void setCurrentData(int r_data[])
+	{
+		currentData = r_data;
+	}
+	public void setOldData(int r_data[])
+	{
+		oldData = r_data;
+	}
+	
+	public void attach(ChangeListener e)
+	{
+		theListeners.add(e);
+	}
+	
+	public void update()
+	{
+		for(ChangeListener e: theListeners)
+		{
+			e.stateChanged(new ChangeEvent(this));
+		}
+	}
+	//checks if a pit is empty
+	public boolean isEmpty(int index)
+	{
+		if(currentData[index]==0)
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	// checks if the game is ended
+	public boolean gameEnd()
+	{
+		// if either bottom pits or top pits are empty, the game is ended
+		if(getBottomStones() == 0 || getTopStones() == 0)
+			return true;
+		// if not, then game is not ended yet
+		else
+			return false;
+	}
+	// collects all the bottom stones
+	public int getBottomStones()
+	{
+		int result = 0;
+		for(int i=0; i<6; i++)
+		{
+			result = currentData[i] + result;
+		}
+		return result;
+	}
+	// collects all the top stones
+	public int getTopStones()
+	{
+		int result = 0;
+		for(int i=7; i<13; i++)
+		{
+			result = currentData[i] + result;
+		}
+		return result;
+	}
+	// puts all leftover stones into correct player's pit
+	public void afterGameEnd()
+	{
+		// collect leftover stones in the bottom row
+		int player1Stones = getBottomStones();
+		// if there is leftover, put them into player1's pit
+		if(player1Stones != 0)
+		{
+			currentData[6] = player1Stones;
+			for(int i=0; i<6; i++)
+			{
+				currentData[i] = 0;
+			}
+		}
+		// collect leftover stones in the top row
+		int player2Stones = getTopStones();
+		// if there is leftover, put them into player2's pit
+		if(player2Stones != 0)
+		{
+			currentData[13] = player2Stones;
+			for(int i=7; i<13; i++)
+			{
+				currentData[i] = 0;
+			}
+		}
+	}
+	// saves currentData into oldData
+	public void saveCurrentData()
+	{
+		oldData = currentData;
+	}
+	// restores oldData into currentData
+	public void undo()
+	{
+		currentData = oldData;
+	}
+	//
+}
+*/
--- src/MancalaView.java
+++ src/MancalaView.java
@@ -103,3 +103,142 @@ public class MancalaView extends JPanel implements ChangeListener {
 	}
 	
 }
+/*
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class MancalaView extends JFrame implements ChangeListener 
+{
+	private MancalaModel theModel;
+	
+	//this is the view AND controller portions
+	
+	//the construcor will also run through the style selection and number of starting stones
+	//implement later
+	MancalaView(MancalaModel r_model)
+	{
+		theModel = r_model;
+		
+		this.setLayout(new FlowLayout());
+		this.setSize(1000, 1000); //this is the size of our overall frame
+		createBoard();
+	}
+	public MancalaModel getModel()
+	{
+		return theModel;
+	}
+	public void setModel(MancalaModel r_model)
+	{
+		theModel = r_model;
+	}
+	@Override
+	public void stateChanged(ChangeEvent c) 
+	{
+			
+	}
+
+	public void createBoard()
+	{
+		//ADD BUTTONS IN THE FOR LOOP BELOW 
+		 //USE FLOWLAYOUT FOR THE STONES
+		
+		JPanel theBoard = new JPanel(); //the board is the mancala board
+		theBoard.setLayout(new BorderLayout());
+		theBoard.setSize(800, 800);
+		
+		JPanel bottomPits = new JPanel();  //this panel is for holding the bottomPits, put into theBoard
+		bottomPits.setLayout(new FlowLayout());
+		
+		JPanel topPits = new JPanel(); //this panel is for holding the topPits, put into theBoard
+		topPits.setLayout(new FlowLayout());
+		
+		JPanel rightPit = new JPanel(); //this panel is for holding player1's stones
+		rightPit.setLayout(new FlowLayout());
+		
+		JPanel leftPit = new JPanel(); //this panel is for holding player2's stones
+		leftPit.setLayout(new FlowLayout());
+		
+		for(int i = 0; i < theModel.getCurrentData().length; i++) //should be i<14
+		{
+			if(i < 6) //this is a score pit
+			{
+				JButton b = new JButton();//this is the button to be added
+				//LATER MUST ADD FUNCTIONALITY TO THE BUTTON. USE ACTION LISTENER
+				//THESE BOTTOM PITS MUST MAKE STONE TRAVEL RIGHT
+				
+				JPanel stoneDisplay = new JPanel();  //this displays the stones inside the pits
+				stoneDisplay.setLayout(new FlowLayout());
+				b.add(stoneDisplay); //the button now has correctly displayed stones inside
+				
+				bottomPits.add(b); //the bottom row now has a correctly done pit.
+			}
+			if(i > 6 && i < 13) //greater than 6 because gotta skip player 1 score pit and less than 13 because 13 is player 2's score pit
+			{
+				JButton b = new JButton();//this is the button to be added
+				//LATER MUST ADD FUNCTIONALITY TO THE BUTTON. USE ACTION LISTENER
+				//THESE TOP PITS MUST MAKE STONES TRAVEL LEFT
+				
+				JPanel stoneDisplay = new JPanel();  //this displays the stones inside the pits
+				stoneDisplay.setLayout(new FlowLayout());
+				b.add(stoneDisplay); //the button now has correctly displayed stones inside
+				
+				topPits.add(b); //the bottom row now has a correctly done pit.
+			}
+			
+		} //end of making the bottom and top pits
+		
+		//making number display here
+		JPanel middleDisplay = new JPanel(); //this is used to display the pit labels for both sides
+		GridLayout middleDisplayLayout = new GridLayout(0,6); //the layout, will display the pitnumbers in grid format
+		middleDisplayLayout.setHgap(5); //makes sure there is space between numbers
+		middleDisplay.setLayout(middleDisplayLayout);
+		
+		for(int i = 7; i < 13; i++) //this loop is putting in the numbers
+		{
+			String number = "" + i;
+			JTextArea num = new JTextArea(number);
+			num.setSize(5, 5);
+			middleDisplay.add(num);
+		}
+		for(int i = 0; i < 6; i++) //this loop is putting in the numbers
+		{
+			String number = "" + i;
+			JTextArea num = new JTextArea(number);
+			num.setSize(5, 5);
+			middleDisplay.add(num);
+		}
+		
+		//end of  number display making
+		
+		theBoard.add(bottomPits, BorderLayout.SOUTH);
+		theBoard.add(topPits, BorderLayout.NORTH);
+		theBoard.add(rightPit, BorderLayout.EAST);
+		theBoard.add(leftPit, BorderLayout.WEST);
+		theBoard.add(middleDisplay, BorderLayout.CENTER);
+		
+		this.add(theBoard);
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	
+	
+	
+}
+*/
